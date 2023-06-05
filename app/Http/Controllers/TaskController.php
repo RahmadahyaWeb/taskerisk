77270eba5c828ce9f603796875d47f0712e201b9
RahmadahyaWeb <thelastpc24@gmail.com>
2023-06-06 00:54:37 +0700

    fix mdidleware & add logout feature

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TaskRequest;
 use App\Models\Task;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 class TaskController extends Controller
 {
--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -14,7 +14,7 @@ class AppLayout extends Component
     public $title;
     public function __construct($title = null)
     {
-        $this->title = $title ?? 'Rahmadahya';
+        $this->title = $title ?? 'Taskerisk';
     }
 
     /**
